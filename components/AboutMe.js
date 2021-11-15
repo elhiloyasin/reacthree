b5cc20444cd1d4b95cc52bb5c116047f7a288826
Yasin Elhilo <elhiloyasin@gmail.com>
2021-11-14 23:47:20 -0500

    Updates

---
+++ components/AboutMe.js
@@ -0,0 +1,20 @@
+import react from 'react';
+
+function AboutMe() {
+    return (
+
+
+       <div className='intro'>
+         <h1>Hello my name is Yaze yooo</h1>
+        </div>
+        
+
+   
+     
+
+    )
+}
+
+
+
+export default AboutMe
---
+++ components/Projects.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function Projects() {
+    return (
+
+        <>
+      
+        <h1>this is my project section not done yet lol</h1>
+
+
+        </>
+
+    )
+
+}
+
+
+export default Projects
