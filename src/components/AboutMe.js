064bf255bf3ab0ccc7ad9440fc316c7d836a9797
Yasin Elhilo <elhiloyasin@gmail.com>
2021-11-16 14:52:04 -0500

    array made

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,33 @@
-import react from 'react';
+
 import { Container, Row, Col } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import Yaze from '../images/yaze.jpg';
-import *  from '../images';
+import Bootstrap from '../images/bootstrap.png';
+import Css from '../images/css.png';
+import github from '../images/github.png';
+import html from '../images/html.png';
+import javascript from '../images/javascript.png';
+import jquery from '../images/jquery.png';
+import mongodb from '../images/mongodb.png';
+import mern from '../images/mern.jpg';
+import mysql from '../images/mysql.jpg';
+import node from '../images/node.png';
+import nodepackage from '../images/nodepackage.png';
+import reactjs from '../images/reactjs.png';
+import express from '../images/express.png';
+
+
+
+
+
+
+
+
+const imagesArray = [Bootstrap, Css, express, github, html, javascript, jquery, mern, mongodb, mysql, node, nodepackage, reactjs];
+
+
+ {console.log(imagesArray[1])}
+
 
 
 function AboutMe() {
@@ -31,6 +56,8 @@ function AboutMe() {
                 <menu id="skills">
             <div className='skills-title'>
             <h1>Skills</h1>
+         
+
             </div>
             <div className='icons'>
 
