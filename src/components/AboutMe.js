69e13d865e931f619e56302641ab90fd31511a24
Yasin Elhilo <elhiloyasin@gmail.com>
2021-11-15 20:15:08 -0500

    need animation for biography add skillsets.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,22 +12,19 @@ function AboutMe() {
                 <h1>About Me</h1>
             </div>
             <Container>
+
                 <Row className="justify-content-md-center">
                     <Col xs={6} md={4}>
-                        <Image  id='image'src={Yaze} thumbnail />
+                        <Image  
+                        id='image'
+                        src={Yaze} 
+                        thumbnail />
                     </Col>
                 </Row>
 
-
                 <div className='bio-info'>
                     <p>
-                        Hello there, I'm Kelvin, a former Divison One Track and Field athlete that turned into a Full Stack Software Engineer. I spend most of my time learning ways to improve and implement a plethora of gained skills, adding value throughout professional endeavors. As Michael Josephson once stated, "Life never stops teaching. Be sure you never stop learning." Ever since I began learning programming I grew a passion for learning that I’ve made it into a daily habit. With technology growing at a faster pace than the speed of light, there is still an abundance of technologies I am willing to learn in the future and especially in the work field.
-
-
-                        If there's anything amazing about programming, it is the ability to create anything imagined. It's challenging, fulfilling, and of course a major headache yet worth every ounce of effort.
-
-
-                        In my off days of coding, I enjoy spontaneous adventures, whiskey, video games, listening to podcasts, working out and sports. I love mentoring upcoming software engineers with a strong passion like myself and lead them to a bright future. I am fluent in Spanish and English. Feel free to communicate with me whichever language you feel comfortable.
+                        Hello there, I'm Yasin Elhilo, a software engineer with a deep passion for software engineering. Lover of learning and staying up-to-date on the latest industry trends and advancements. Excited to leverage a thirst for knowledge and a versatile skill set to further business objectives while exceeding quality standards.
                     </p>
 
                 </div>
