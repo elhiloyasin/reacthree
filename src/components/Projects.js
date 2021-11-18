b5b593c8389e73872b6635a7573d49a2637d0c7c
Yasin Elhilo <elhiloyasin@gmail.com>
2021-11-18 05:33:47 -0500

    working on projects link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import {Row, Container, Col} from 'react-bootstrap/';
+import Homegrown from '../images/homegrown.png';
+
 
 
 
@@ -13,13 +18,38 @@ function Projects() {
     return (
 
         <>
+             <div>
+                <div className='projects-title'>
+                    <h1>Projects</h1>
+                </div>
+                <div className='frontend-title'>
+                    <h2>FRONT-END</h2>
+                </div>
+            </div>
 
-            <div className='projects-title'>
-                <h1>Projects</h1>
-            </div>
-            <div className='frontend-title'>
-                <h2>FRONT-END</h2>
-            </div>
+            <Container className="text-white content-center justify-content-md-center">
+                <Row>
+                    <Col className="flex-end">
+                    <Card.Img src={Homegrown} alt="homegrown" />
+                    </Col>
+                    <Col className="justify-content-md-center">
+                    <Card.Body >
+                    <Card.Title className="frontendone-title"> HOME GROWN </Card.Title>
+                    <Card.Text className='frontendone-description'>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Primary</Button>
+                    <Button variant="primary">Primary</Button>{' '}
+                </Card.Body>
+                    </Col>
+                </Row>
+            </Container>
+
+
+
+
+
         </>
 
     )
