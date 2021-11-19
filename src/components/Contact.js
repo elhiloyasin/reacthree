6a855281caeef2677f1f045f952d4834f01e0fd0
Yasin Elhilo <elhiloyasin@gmail.com>
2021-11-19 18:58:05 -0500

    adding changes to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Row, Container, Col } from 'react-bootstrap/';
+import Card from 'react-bootstrap/Card';
 
 
 function Contact() {
     return (
- <>
+        <>
 
-        
-</>
+            <div className='contact-tile'>
+                <h1>Contact Me!</h1>
+            </div>
+
+            <Container>
+                <Row className="row">
+                    <Col >
+                    <Card.Body className='mx-auto'>This is some text within a card bodydsfasdfasdfadsfasdfasdfasdfasdf.</Card.Body>
+                    <Card.Body>This is some text within a card body.</Card.Body>
+                    <Card.Body>This is some text within a card body.</Card.Body>
+                    <Card.Body>This is some text within a card body.</Card.Body>
+                    <Card.Body>This is some text within a card body.</Card.Body>
+                    <Card.Body>This is some text within a card body.</Card.Body>
+
+                    <Card.Body>This is some text within a card body.</Card.Body>
+
+
+
+                    </Col>
+                    <Col>
+
+                    <Card.Body>This is some text within a card bodydsfasdfasdfadsfasdfasdfasdfasdf.</Card.Body>
+                    </Col>
+
+
+
+                </Row>
+
+
+
+            </Container>
+
+
+        </>
     )
 
 }
